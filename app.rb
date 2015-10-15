3be0b59e640d96b75e44ef3f39de4800fd4bfbe0
Gary Wong <garywong79@me.com>
2015-10-15 14:23:35 +0800

    change table

---
+++ app.rb
@@ -0,0 +1,7 @@
+require_relative 'app/models/student.rb'
+
+p Student.column_names
+# p Student.count
+
+# ["id", "first_name", "last_name", "gender", "birthday", "email", "phone"]
+# 108
--- db/migrate/20151015141533_concatenate_name_to_students.rb
+++ db/migrate/20151015141533_concatenate_name_to_students.rb
@@ -2,8 +2,12 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to 
 
-class CreateStudents < ActiveRecord::Migration
+class ConcatenateNameToStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    change_table :students do |t|
+        t.remove :first_name, :last_name
+        t.string :name
+    end
   end
 end
